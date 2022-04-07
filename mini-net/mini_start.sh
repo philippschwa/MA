81ed74e6965e0f7512b3bea3d82b3af2924c783b
philippschwa <philipp.schwabenbauer@stud.uni-regensburg.de>
2022-04-07 15:50:53 +0200

    MiniNet und Wazuh Manager running.

--- mini-net/mini_start.sh
+++ mini-net/mini_start.sh
@@ -4,5 +4,7 @@ echo "Inside mini_start!"
 
 service openvswitch-switch start
 #mn -c
-mn --custom ./mininet/custom/topo-2sw-2host.py --topo mytopo --test pingall
+#mn --custom ./mininet/custom/topo-2sw-2host.py --topo mytopo --test pingall
 #python3 test.py
+
+tail -f /dev/null
--- twin/deploy.sh
+++ twin/deploy.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-#### WAZUH AGENT INSTALLATION ####
-
-# Add the Wazuh repository to download the official packages
-curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
-echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
-apt-get update
-
-# Setting hostname and install agent
-echo "INSTALL WAZUH AGENT"
-WAZUH_MANAGER="wazuh-manager" apt-get install -y wazuh-agent
-#echo "MOVE CONFIGURATION FILE ossec.conf"
-#mv ./ossec.conf /var/ossec/etc/ossec.conf
-
 # Enable and start Wazuh agent service
 echo "SYSTEMCTL COMMANDS"
 systemctl daemon-reload
 systemctl enable wazuh-agent
-systemctl start wazuh-agent
+service wazuh-agent start
 tail -n 10 /var/ossec/logs/ossec.log
-echo "WAZUH UP"
+echo "WAZUH AGENT IS RUNNING"
 
-#### MINI NET INSTALLATION ####
+# Start MiniNet
+service openvswitch-switch start
+
+# Dummy process to keep container running
 tail -f /dev/null
