0fb72c7948880a8d8fcd059144e8599668123680
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-07-07 17:58:38 +0200

    asdf

--- docker-ns3-tap/scripts/install_suri_agent.sh
+++ docker-ns3-tap/scripts/install_suri_agent.sh
@@ -4,13 +4,13 @@
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add - 
 echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list 
 apt-get update 
-apt-get install -y wazuh-agent
+WAZUh_MANAGER="123.123.0.2" apt-get install -y wazuh-agent
 
 # install suricata and update rules
 apt install -y software-properties-common 
 apt-get update
 add-apt-repository ppa:oisf/suricata-stable 
-apt-get update && apt-get install -y suricata iproute2 
+apt-get update && apt-get install -y suricata iproute2 jq
 
 # suricata-update -o /etc/suricata/rules
 
@@ -21,7 +21,13 @@ cp ../rules/local.rules /etc/suricata/rules/local.rules
 cp ../rules/suricata.rules /etc/suricata/rules/suricata.rules
 
 # start suricata service --> bridge node1, later all bridges used in simulation
+systemctl daemon-reload
+systemctl enable suricata
+systemctl start suricata
+
 suricata -i br-node1 -D
 
 # start wazuh agent
-/var/ossec/bin/wazuh-control start
+systemctl daemon-reload
+systemctl enable wazuh-agent
+systemctl start wazuh-agent
