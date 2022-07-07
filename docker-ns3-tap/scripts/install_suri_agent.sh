540e284ad7981f1cabd9681f33278534632c29bf
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-07-07 18:30:12 +0200

    suricata setup

--- docker-ns3-tap/scripts/install_suri_agent.sh
+++ docker-ns3-tap/scripts/install_suri_agent.sh
@@ -5,6 +5,7 @@ curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
 echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list 
 apt-get update 
 WAZUh_MANAGER="123.123.0.2" apt-get install -y wazuh-agent
+echo "wazuh-agent hold" | dpkg --set-selections
 
 # install suricata and update rules
 apt install -y software-properties-common 
@@ -17,15 +18,15 @@ apt-get update && apt-get install -y suricata iproute2 jq
 # copy custom config files for agent and suricata
 cp ../config/suricata.yaml /etc/suricata/suricata.yaml
 cp ../config/ossec.conf /var/ossec/etc/ossec.conf
-cp ../rules/local.rules /etc/suricata/rules/local.rules
-cp ../rules/suricata.rules /etc/suricata/rules/suricata.rules
+cp ../rules/ /etc/suricata/rules/
 
 # start suricata service --> bridge node1, later all bridges used in simulation
 systemctl daemon-reload
 systemctl enable suricata
 systemctl start suricata
 
-suricata -i br-node1 -D
+# sollte über config datei einstellbar sein, welche bridges gemonitored werden sollen
+#suricata -i br-node1 -D
 
 # start wazuh agent
 systemctl daemon-reload
