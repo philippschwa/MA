c38e5e284a7299388ca8aebc0c037d2b1d18073d
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-07-07 17:07:48 +0200

    suricata & wazuh agent

---
+++ docker-ns3-tap/scripts/install_suri_agent.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# download and install wazuh agent
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add - 
+echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list 
+apt-get update 
+apt-get install -y wazuh-agent
+
+# install suricata and update rules
+apt install -y software-properties-common 
+apt-get update
+add-apt-repository ppa:oisf/suricata-stable 
+apt-get update && apt-get install -y suricata iproute2 
+
+# suricata-update -o /etc/suricata/rules
+
+# copy custom config files for agent and suricata
+cp ../config/suricata.yaml /etc/suricata/suricata.yaml
+cp ../config/ossec.conf /var/ossec/etc/ossec.conf
+cp ../rules/local.rules /etc/suricata/rules/local.rules
+cp ../rules/suricata.rules /etc/suricata/rules/suricata.rules
+
+# start suricata service --> bridge node1, later all bridges used in simulation
+suricata -i br-node1 -D
+
+# start wazuh agent
+/var/ossec/bin/wazuh-control start
--- install_packages.sh
+++ install_packages.sh
@@ -2,8 +2,9 @@
 # Setup "empty" VM -- install dependencies and ns3 (ns3 has to be run as non sudo)
 
 apt-get update -y && apt-get upgrade -y
-apt install -y g++ python3 python3-pip cmake python3-setuptools git tcpdump uml-utilities bridge-utils
+apt install -y g++ python3 python3-pip cmake python3-setuptools git tcpdump uml-utilities bridge-utils iproute2
 
 # vtun lxc
 
-sudo -u caesar git clone https://gitlab.com/nsnam/ns-3-allinone.git && cd ns-3-allinone && ./download.py -n ns-3.36 && ./build.py 
+# funktioniert so nicht, da immernoch sudo meldung kommt..
+git clone https://gitlab.com/nsnam/ns-3-allinone.git && cd ns-3-allinone && ./download.py -n ns-3.36 && ./build.py 
