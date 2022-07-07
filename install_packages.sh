2235b359d110803b05046577f179328a0302a3d2
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-07-07 11:54:45 +0200

    ip addr fix

--- install_packages.sh
+++ install_packages.sh
@@ -2,7 +2,7 @@
 # Setup "empty" VM -- install dependencies and ns3 (ns3 has to be run as non sudo)
 
 apt-get update -y && apt-get upgrade -y
-apt install -y g++ python3 cmake python3-setuptools git tcpdump uml-utilities bridge-utils
+apt install -y g++ python3 python3-pip cmake python3-setuptools git tcpdump uml-utilities bridge-utils
 
 # vtun lxc
 
