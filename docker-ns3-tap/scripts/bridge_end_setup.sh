a60cfb148d42bc9710b8b8d498932cec99fa5eae
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-07-07 16:05:13 +0200

    clean up

--- docker-ns3-tap/scripts/bridge_end_setup.sh
+++ docker-ns3-tap/scripts/bridge_end_setup.sh
@@ -12,9 +12,6 @@
 sysctl net.bridge.bridge-nf-call-iptables=0
 sysctl net.bridge.bridge-nf-call-arptables=0
 sysctl net.bridge.bridge-nf-call-ip6tables=0
-sysctl net.bridge.bridge-nf-call-ip6tables=0
-
-
 
 status=$?
 if [ $status -ne 0 ]; then
