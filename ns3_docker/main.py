84384267cf482f3392eb20d2675f27a248a37137
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-08-04 11:17:06 +0200

    asdf

--- ns3_docker/main.py
+++ ns3_docker/main.py
@@ -134,8 +134,8 @@ def startNs3():
     print ("Yolo")
     print("Starting ns3 Simulation. It is active for 10 minutes.")
 
-    subprocess.Popen("cd %s && ./ns3 run --enable-sudo scratch/sim_topo.cc" % (ns3_path), shell=True)
-
+    #subprocess.Popen("cd %s && ./ns3 run --enable-sudo scratch/sim_topo.cc" % (ns3_path), shell=True)
+    subprocess.Popen("cd %s && ./ns3 run --enable-sudo scratch/test.cc" % (ns3_path), shell=True)
 
 
 def setup_new():
