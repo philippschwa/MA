27d25b51363ab78dd528935f5e1be03da69b1ea1
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-08-12 14:32:07 +0200

    hmi ssh server

---
+++ ns3_docker/docker/volumes/hmi/scripts/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+service ssh start
+
+status=$?
+if [ $status -ne 0 ]; then
+    echo "Failed to execute deploy script!"
+    exit $status
+fi
+
+# Dummy process to keep container running
+tail -f /dev/null
