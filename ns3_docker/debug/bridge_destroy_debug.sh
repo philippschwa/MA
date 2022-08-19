e20382965de654719cbea2e97de6ba32a6047dee
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-08-19 16:41:52 +0200

    asdf

--- ns3_docker/debug/bridge_destroy_debug.sh
+++ ns3_docker/debug/bridge_destroy_debug.sh
@@ -9,21 +9,12 @@ fi
 NAME=$1
 BR_NAME=br-$NAME
 TAP_NAME=tap-$NAME
-VETH1=veth1-$NAME
 
-
+# Remove tap intefaces and bridges
 sudo ip link set $BR_NAME down
 sudo ip link set $TAP_NAME down
 sudo ip link del $TAP_NAME
-sudo ip link del $VETH1
 sudo ip link del $BR_NAME
-
-# Old commands. Using newer iproute2 is better
-#sudo ifconfig $BR_NAME down
-#sudo brctl delif $BR_NAME $TAP_NAME
-#sudo brctl delbr $BR_NAME
-#sudo ifconfig $TAP_NAME down
-#sudo tunctl -d $TAP_NAME
 
 status=$?
 if [ $status -ne 0 ]; then
